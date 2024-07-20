36b29e9a472e6933fb4a7547b42d93095ca6470c
ElizabethLanyl <elizabethlan0926@gmail.com>
2024-07-20 17:46:45 +0800

    Update deploy.bat

--- jenkins/scripts/deploy.bat
+++ jenkins/scripts/deploy.bat
@@ -8,6 +8,6 @@ REM Copy project files to htdocs
 xcopy /E /I /Y C:\ProgramData\Jenkins\.jenkins\workspace\lab07b\src C:\xampp\htdocs\jenkins-php-selenium-test
 
 echo Deployment completed.
-echo Visit http://localhost:81/my-apache-php-app to see your PHP application in action.
+echo Visit http://localhost:81/jenkins-php-selenium-test to see your PHP application in action.
 pause
 
