c7697406b2cb76c5d97bcc72814f1811b8597ec1
ElizabethLanyl <elizabethlan0926@gmail.com>
2024-07-20 17:28:22 +0800

    test

--- jenkins/scripts/deploy.bat
+++ jenkins/scripts/deploy.bat
@@ -5,7 +5,7 @@ REM Start Apache server on localhost using XAMPP
 "C:\xampp\xampp_start.exe"
 
 REM Copy project files to htdocs
-xcopy /E /I /Y C:\xampp\htdocs\jenkins-php-selenium-test
+xcopy /E /I /Y C:\ProgramData\Jenkins\.jenkins\workspace\lab07b\src C:\xampp\htdocs\jenkins-php-selenium-test
 
 echo Deployment completed.
 echo Visit
